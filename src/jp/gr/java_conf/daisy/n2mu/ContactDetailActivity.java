29f6e949af1bbfa5cbf719c58f2d1949f34e900c
daisy1754 <d-part@hotmail.co.jp>
2014-10-11 17:27:17 -0700

    Add tab to contact detail view

--- src/jp/gr/java_conf/daisy/n2mu/ContactDetailActivity.java
+++ src/jp/gr/java_conf/daisy/n2mu/ContactDetailActivity.java
@@ -1,9 +1,19 @@
 package jp.gr.java_conf.daisy.n2mu;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class ContactDetailActivity extends Activity {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+import com.viewpagerindicator.TabPageIndicator;
+import com.viewpagerindicator.TitlePageIndicator;
+
+public class ContactDetailActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,5 +22,41 @@ public class ContactDetailActivity extends Activity {
         getActionBar().setDisplayShowHomeEnabled(false);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setTitle("Profile");
+
+        ViewPager pager = (ViewPager)findViewById(R.id.pager);
+        pager.setAdapter(new ContactInfoAdapter(getSupportFragmentManager()));
+        TabPageIndicator tabIndicator = (TabPageIndicator)findViewById(R.id.titles);
+        tabIndicator.setViewPager(pager);
+    }
+
+    private class ContactInfoAdapter extends FragmentPagerAdapter {
+        private final String[] CONTENT = new String[] { "Summary", "Linkedin", "Twitter" };
+        private int mCount = CONTENT.length;
+
+        public ContactInfoAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new ContactSummaryFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return mCount;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return CONTENT[position];
+        }
+
+        public void setCount(int count) {
+            if (count > 0 && count <= 10) {
+                mCount = count;
+                notifyDataSetChanged();
+            }
+        }
     }
 }
---
+++ src/jp/gr/java_conf/daisy/n2mu/ContactSummaryFragment.java
@@ -0,0 +1,15 @@
+package jp.gr.java_conf.daisy.n2mu;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ContactSummaryFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contact_summary, container, false);
+    }
+}
